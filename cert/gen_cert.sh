912f60e4ac190d9f21d15bcc9a5f10fc50e38baa
marcin <m.lojewski1998@gmail.com>
2024-07-29 22:35:38 +0200

    fix cert subject, add SANs

--- cert/gen_cert.sh
+++ cert/gen_cert.sh
@@ -7,10 +7,9 @@ openssl genpkey -out priv.key -outpubkey pub.key -algorithm RSA -pkeyopt rsa_key
 openssl genpkey -out ca-priv.key -outpubkey ca-pub.key -algorithm RSA -pkeyopt rsa_keygen_bits:4096
 
 # 3. Generate root CA certificate
-openssl x509 -new -key ca-priv.key -subj /DC=CA/DC=RootCA/DC=RootCA/UID=111111+CN=RootCA -out ca.cert
-
+openssl x509 -new -key ca-priv.key -subj "/C=PL/ST=State/L=City/O=Company/OU=Section/CN=root.ca.com" -out ca.cert
 # 4. Generate server CSR
-openssl req -new -key priv.key -subj /DC=ServCert/DC=ServCert/DC=ServCert/UID=222222+CN=ServCert -out serv-cert.req
+openssl req -new -key priv.key -subj "/C=PL/ST=State/L=City/O=Company/OU=Section/CN=chat-ai-assistant" -addext "subjectAltName = DNS:chat.grpc-server.com"  -out serv-cert.req
 
 # 5. Sign server CSR with root CA
 openssl req -in serv-cert.req -x509 -copy_extensions copyall -CA ca.cert -CAkey ca-priv.key -out serv.cert
